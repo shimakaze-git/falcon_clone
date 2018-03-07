451a605f70fab04faccad9f7959a118897c002ba
shimakaze-git <shimakaze.soft+github@googlemail.com>
2018-03-07 23:05:02 +0000

    add requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import json
 import falcon
 
+class AppResource(object):
+    
+    def on_get(self, req, resp):
+        
+        msg = {
+            "message": "Welcome to the Falcon"
+        }
+        resp.body = json.dumps(msg)
+
 app = falcon.API()
-print(app)
+app.add_route("/", AppResource())
+
+if __name__ == "__main__":
+    from wsgiref import simple_server
+    httpd = simple_server.make_server("127.0.0.1", 8000, app)
+    httpd.serve_forever()
